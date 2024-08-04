56679a5154c829dd8652fcb76f55c195b1f68ae4
Enkidu275 <75416275+Enkidu275@users.noreply.github.com>
2024-08-04 10:45:13 -0600

    Update

---
+++ eternalafterlife.cpp
@@ -0,0 +1,7 @@
+const int BODYsignal=0;
+//first bit is most recent, shift right as emitted or not.
+const int BODYneutral=1;
+//comparison condition, looks through dendrite stack, if only 5 dendrites, last 3 bits should be 0
+
+const DENDRITEpointer=0;//points to neuron body
+const 
